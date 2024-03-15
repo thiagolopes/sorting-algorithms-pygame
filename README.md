5c0644c68ac998eba94cbf2e09ace3ef7c24cfa9
thiagolopes <thiagolopes@protonmail.com>
2024-03-15 16:47:38 -0300

    Now with step by step draw, with other colors

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 Video 1: https://www.youtube.com/watch?v=pXIVzfX-9vs
 
-![image](https://github.com/thiagolopes/sorting-algorithms-pygame/assets/5994972/f3d1e3e2-3b6e-494b-a1ff-c1caf2e67b2b)
+![image](https://github.com/thiagolopes/sorting-algorithms-pygame/assets/5994972/f66e273f-2c9c-4b35-a8da-8505684924cf)
+![image](https://github.com/thiagolopes/sorting-algorithms-pygame/assets/5994972/572b4c8a-f83e-4fd2-b1a8-9998bb7c1035)
 
 References:
 - PyGame Docs - (https://www.pygame.org/docs/)
--- sort.py
+++ sort.py
@@ -1,5 +1,5 @@
-import sys
 import math
+import sys
 from random import shuffle
 
 import pygame
@@ -17,24 +17,48 @@ running = True
 dt = 0
 
 
+def quit():
+    pygame.quit()
+    sys.exit()
+
+
 class BubbleSort:
     def __init__(self, elements):
         self.elements = elements
         self.steps_left = len(self.elements)
+        self.index_step = 1
+        self.dirties = []
+        self.finished = True
 
     def step(self):
+        if self.steps_left == 0:
+            self.finish()
+            return False
+
+        if self.index_step == self.steps_left:
+            self.index_step = 1
+            self.steps_left -= 1
+            return False
+
+        i = self.index_step
         swapped = False
+        if elements[i - 1] > elements[i]:
+            elements[i - 1], elements[i] = elements[i], elements[i - 1]
+            swapped = True
+            self.dirties = [i, i - 1]
 
-        for i in range(1, self.steps_left):
-            if elements[i - 1] > elements[i]:
-                elements[i - 1], elements[i] = elements[i], elements[i - 1]
-                swapped = True
-        self.steps_left -= 1
-
+        self.index_step += 1
         return swapped
+
+    def finish(self):
+        self.dirties = []
+        self.finished = True
 
     def reset(self):
         self.steps_left = len(self.elements)
+        self.index_step = 1
+        self.finished = False
+        self.dirties = []
         shuffle(self.elements)
 
 
@@ -42,29 +66,44 @@ def play_beep():
     pygame.mixer.Sound.play(beep)
 
 
-def quit():
-    pygame.quit()
-    sys.exit()
+class Grid:
+    def __init__(self, pos, size, screen):
+        self.pos = pos
+        self.size = size
+        self.surface = pygame.Surface(self.size)
+        self.screen = screen
+
+    def draw(self, elements, indexes_dirty, finished):
+        self.surface.fill("black")
+
+        top = self.size.x / len(elements)
+        left = self.size.y / len(elements)
+        offset = self.size.y
+
+        width = math.floor(top) - 1 if top - 1 > 1 else 1
+        height = math.ceil(left) if left > 1 else 1
+
+        for i, element in enumerate(elements):
+            pos = pygame.Rect(i * top, offset - (element * left), width, height * element)
+            if finished:
+                self.surface.fill("green", pos)
+                continue
+            if i in indexes_dirty:
+                self.surface.fill("blue", pos)
+            else:
+                self.surface.fill("white", pos)
+
+        self.screen.blit(self.surface, self.pos)
 
 
-def draw_grid(elements, pos):
-    top = pos.x / len(elements)
-    left = pos.y / len(elements)
-    offset = pos.y
-
-    # 1px min
-    width = math.ceil(top) if top > 1 else 1
-    height = math.ceil(left) if left > 1 else 1
-
-    for i, element in enumerate(elements):
-        pos = pygame.Rect(i * top, offset - (element * left), width, height * element)
-        pygame.draw.rect(screen, "red", pos)
-
-
-TOTAL = 420
+TOTAL = 128
 elements = list(range(1, TOTAL))
-grid_pos = Vector2(screen.get_width(), screen.get_height())
 bubble = BubbleSort(elements)
+play = False
+
+grid_pos = Vector2(5, 5)
+grid_size = Vector2(screen.get_width() - 10, screen.get_height() - 10)
+grid = Grid(grid_pos, grid_size, screen)
 
 while running:
     for event in pygame.event.get():
@@ -75,18 +114,22 @@ while running:
                 quit()
             if event.key == pygame.K_RETURN:
                 bubble.reset()
+                play = False
+            if event.key == pygame.K_SPACE:
+                play = not play
 
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_SPACE]:
-        success = bubble.step()
-        if success:
+    if play:
+        t = bubble.step()
+        if t:
             play_beep()
+    if bubble.finished:
+        play = False
 
     # Draw
     screen.fill("black")
-    draw_grid(bubble.elements, grid_pos)
+    grid.draw(bubble.elements, bubble.dirties, bubble.finished)
 
     pygame.display.flip()
-    dt = clock.tick(144) / 1000
+    # dt = clock.tick(144) / 1000
 
 pygame.quit()
