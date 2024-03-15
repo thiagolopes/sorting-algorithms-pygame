0cda958ae233b9f724a116eb4d13814ba92dc803
thiagolopes <thiagolopes@protonmail.com>
2024-03-15 14:33:55 -0300

    First refactor - create bubble sort class

--- sort.py
+++ sort.py
@@ -1,24 +1,51 @@
 import sys
+import math
 from random import shuffle
 
 import pygame
 from pygame import Vector2
 
 pygame.init()
+
 beep = pygame.mixer.Sound("beep.wav")
 beep.set_volume(0.05)
 screen = pygame.display.set_mode((1280, 720))
 pygame.display.set_caption("Bubble sort - <space> sort, <enter> reset")
+
 clock = pygame.time.Clock()
 running = True
 dt = 0
 
+
+class BubbleSort:
+    def __init__(self, elements):
+        self.elements = elements
+        self.steps_left = len(self.elements)
+
+    def step(self):
+        swapped = False
+
+        for i in range(1, self.steps_left):
+            if elements[i - 1] > elements[i]:
+                elements[i - 1], elements[i] = elements[i], elements[i - 1]
+                swapped = True
+        self.steps_left -= 1
+
+        return swapped
+
+    def reset(self):
+        self.steps_left = len(self.elements)
+        shuffle(self.elements)
+
+
 def play_beep():
     pygame.mixer.Sound.play(beep)
+
 
 def quit():
     pygame.quit()
     sys.exit()
+
 
 def draw_grid(elements, pos):
     top = pos.x / len(elements)
@@ -26,18 +53,18 @@ def draw_grid(elements, pos):
     offset = pos.y
 
     # 1px min
-    height = left if left > 1 else 1
-    width = top if top > 1 else 1
+    width = math.ceil(top) if top > 1 else 1
+    height = math.ceil(left) if left > 1 else 1
 
     for i, element in enumerate(elements):
         pos = pygame.Rect(i * top, offset - (element * left), width, height * element)
         pygame.draw.rect(screen, "red", pos)
 
+
 TOTAL = 420
 elements = list(range(1, TOTAL))
-step = len(elements)
-# shuffle(elements)
 grid_pos = Vector2(screen.get_width(), screen.get_height())
+bubble = BubbleSort(elements)
 
 while running:
     for event in pygame.event.get():
@@ -47,20 +74,17 @@ while running:
     screen.fill("black")
 
     # draw grid
-    draw_grid(elements, grid_pos)
+    draw_grid(bubble.elements, grid_pos)
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_ESCAPE]:
         quit()
     if keys[pygame.K_SPACE]:
-        for i in range(1, step):
-            if elements[i - 1] > elements[i]:
-                elements[i - 1], elements[i] = elements[i], elements[i - 1]
-                play_beep()
-        step -= 1
+        success = bubble.step()
+        if success:
+            play_beep()
     if keys[pygame.K_RETURN]:
-        step = len(elements)
-        shuffle(elements)
+        bubble.reset()
 
     pygame.display.flip()
     dt = clock.tick(144) / 1000
