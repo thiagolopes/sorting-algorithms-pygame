57e68df6c62ef4646b89a170230421a43d880403
thiagolopes <thiagolopes@protonmail.com>
2024-03-16 03:00:04 -0300

    Implement full Bar

--- sort.py
+++ sort.py
@@ -4,14 +4,16 @@ import sys
 
 import pygame
 
+W = 780
+H = 1280
 RUNNING = True
 START = 0
 TOTAL = 64
-elements = list(range(START, TOTAL))
+ELEMENTS = list(range(START, TOTAL))
 
 
 def quit():
-    print(elements)
+    print(ELEMENTS)
     pygame.quit()
     sys.exit()
 
@@ -25,75 +27,24 @@ class Bar:
         self.pos = pos
         self.screen = screen
         self.surface = pygame.Surface(pygame.Vector2(pos.w, pos.h))
-        self.text = pygame.font.SysFont("Monospace", 18)
+        self.text = pygame.font.SysFont("Monospace", 18, True)
 
-        self.title_pos = pygame.Vector2(2, 0)
+        self.margin = 8
+        self.init_pos = pygame.Vector2(2, 0)
 
-    def draw(self, algorithm, meta):
+    def draw_next_text(self, text, pos, color):
+        ts = self.text.render(text, True, color)
+        self.surface.blit(ts, pos)
+        pos += pygame.Vector2(ts.get_width() + self.margin, 0)
+
+    def draw(self, algorithm, meta, ms):
         self.surface.fill("dimgray", self.pos)
-        title = self.text.render(algorithm, True, "green")
-        self.surface.blit(title, self.title_pos)
-
-        meta_text = self.text.render(meta, True, "white")
-        meta_pos = pygame.Vector2(self.title_pos.x + title.get_width() + 4, self.title_pos.y)
-        self.surface.blit(meta_text, meta_pos)
+        pos = self.init_pos.copy()
+        self.draw_next_text(algorithm, pos, "green")
+        self.draw_next_text(meta, pos, "white")
+        self.draw_next_text(f"{ms} ms", pos, "white")
 
         self.screen.blit(self.surface, self.pos)
-
-
-class BubbleSort:
-    def __init__(self, elements):
-        self.elements = elements
-        self.steps_left = len(self.elements) - 1
-        self.index_step = 0
-        self.dirty_index = []
-        self.finished = True
-
-        self.step_count = 0
-
-    def __str__(self):
-        return "Bubble Sort"
-
-    def __repr__(self):
-        return f"< Finished: {self.finished} | Step count: {self.step_count} | Time Lapse: TODO >"
-
-    def step(self):
-        self.dirty_index = []
-
-        if self.finished:
-            return False
-
-        if self.steps_left == 0:
-            self.finish()
-            return False
-
-        if self.index_step == self.steps_left:
-            self.index_step = 0
-            self.steps_left -= 1
-            return False
-
-        i = self.index_step
-        il = i + 1
-        self.dirty_index.append(i)
-        if elements[i] > elements[il]:
-            elements[il], elements[i] = elements[i], elements[il]
-            self.dirty_index.append(il)
-
-        self.index_step += 1
-        self.step_count += 1
-        return True
-
-    def finish(self):
-        self.dirty = []
-        self.finished = True
-
-    def reset(self):
-        self.steps_left = len(self.elements) - 1
-        self.index_step = 0
-        self.dirty_index = []
-        self.finished = False
-        self.step_count = 0
-        random.shuffle(self.elements)
 
 
 class Grid:
@@ -119,12 +70,12 @@ class Grid:
         pos = pygame.Rect(index * top, self.size.y - (element * left), width, height)
         self.surface.fill(color, pos)
 
-    def draw_clear_last_indexes(self):
+    def draw_clear_last_indexes(self, elements):
         for ld in self.last_dirty_indexes:
             self.draw_index(ld, len(elements), elements[ld], "white")
 
     def draw(self, elements, dirty_index, finished):
-        self.draw_clear_last_indexes()
+        self.draw_clear_last_indexes(elements)
 
         if dirty_index:
             for d in dirty_index:
@@ -142,13 +93,69 @@ class Grid:
         self.screen.blit(self.surface, self.pos)
 
 
+class BubbleSort:
+    def __init__(self, elements):
+        self.elements = elements
+        self.steps_left = len(self.elements) - 1
+        self.index_step = 0
+        self.dirty_index = []
+        self.finished = True
+
+        self.step_count = 0
+
+    def __str__(self):
+        return "Bubble Sort"
+
+    def __repr__(self):
+        return f"< Finished: {self.finished} | Step count: {self.step_count} >"
+
+    def step(self):
+        self.dirty_index = []
+
+        if self.finished:
+            return False
+
+        if self.steps_left == 0:
+            self.finish()
+            return False
+
+        if self.index_step == self.steps_left:
+            self.index_step = 0
+            self.steps_left -= 1
+            return False
+
+        elements = self.elements
+        i = self.index_step
+        il = i + 1
+        self.dirty_index.append(i)
+        if elements[i] > elements[il]:
+            elements[il], elements[i] = elements[i], elements[il]
+            self.dirty_index.append(il)
+
+        self.index_step += 1
+        self.step_count += 1
+        return True
+
+    def finish(self):
+        self.dirty = []
+        self.finished = True
+
+    def reset(self):
+        self.steps_left = len(self.elements) - 1
+        self.index_step = 0
+        self.dirty_index = []
+        self.finished = False
+        self.step_count = 0
+        random.shuffle(self.elements)
+
+
 pygame.init()
 beep = pygame.mixer.Sound("beep.wav")
 beep.set_volume(0.05)
-screen = pygame.display.set_mode((1280, 780))
+screen = pygame.display.set_mode((H, W))
 pygame.display.set_caption("Bubble sort - <space> sort, <enter> reset")
 
-bubble = BubbleSort(elements)
+bubble = BubbleSort(ELEMENTS)
 play = False
 
 bar_size = 24
@@ -160,6 +167,7 @@ grid = Grid(grid_pos, grid_size, screen)
 bar_pos = pygame.Rect(0, 0, screen.get_width(), bar_size)
 bar = Bar(bar_pos, screen)
 
+time = 0
 while RUNNING:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -170,30 +178,39 @@ while RUNNING:
             if event.key == pygame.K_RETURN:
                 play = False
                 bubble.reset()
+                time = 0
             if event.key == pygame.K_SPACE:
                 play = not play
             if event.key == pygame.K_n:
                 bubble.step()
             if event.key == pygame.K_BACKSPACE:
                 play = False
-                elements = list(range(START, TOTAL))
-                bubble = BubbleSort(elements)
+                ELEMENTS = list(range(START, TOTAL))
+                bubble = BubbleSort(ELEMENTS)
+                time = 0
             if event.key == pygame.K_r:
                 play = False
-                elements = [random.randint(START, TOTAL) for _ in range(START, TOTAL)]
-                bubble = BubbleSort(elements)
+                ELEMENTS = [random.randint(START, TOTAL) for _ in range(START, TOTAL)]
+                bubble = BubbleSort(ELEMENTS)
                 bubble.finished = False
+                time = 0
 
-    if play:
-        t = bubble.step()
-        if t:
-            play_beep()
     if bubble.finished:
         play = False
 
+    start_time = None
+    if play:
+        start_time = pygame.time.get_ticks()
+        t = bubble.step()
+        if t:
+            play_beep()
+
     # Draw
     grid.draw(bubble.elements, bubble.dirty_index, bubble.finished)
-    bar.draw(str(bubble), repr(bubble))
+    bar.draw(str(bubble), repr(bubble), time)
+
+    if start_time is not None:
+        time += pygame.time.get_ticks() - start_time
 
     pygame.display.flip()
 
